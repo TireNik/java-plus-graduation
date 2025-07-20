7237dc74f557c1df72dbe9d329c341b22ec1cde4
TireNik <sugarman.sviridov@yandex.ru>
2025-07-20 21:05:41 +0300

    Fix fix fix что тут только нет

--- infra/gateway-server/src/main/java/ru/practicum/GatewayApp.java
+++ infra/gateway-server/src/main/java/ru/practicum/GatewayApp.java
@@ -2,10 +2,12 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ConfigurationPropertiesScan
 public class GatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class, args);
